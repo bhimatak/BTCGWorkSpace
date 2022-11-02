e8d5ee26d9c483da1d8ad74db673c12fbc57634a
bhimatak <bhima.t@gmail.com>
2022-11-02 10:58:23 +0530

    Updated

--- Classwork/day03/src/prog02.c
+++ Classwork/day03/src/prog02.c
@@ -5,7 +5,9 @@ int main()
 {
 	char str1[20] = {'\0',};
 	char str2[20] = {'\0', };
+	char str3[20] = { '\0', };
 	int sLength1 = 0, sLength2 = 0;
+	int cmpValue = 0;
 
 	printf("\nEnter string 1: ");
 	scanf("%s",str1);
@@ -16,8 +18,25 @@ int main()
 	sLength1 = strlen(str1);
 	sLength2 = strlen(str2);
 
-	printf("\nString lenght of str1: %d", sLength1);
-	printf("\nString lenght of str2: %d", sLength2);
+	printf("\nString lenght of str1: %d\n", sLength1);
+	printf("\nString lenght of str2: %d\n", sLength2);
+
+	cmpValue = strcmp(str1, str2);
+
+	/*printf("\ncmpValue = %d\n", cmpValue);*/
+
+	if(cmpValue == 0)
+		printf("\nBoth str1 and str2 are same\n");
+	else if(cmpValue < 0)
+		printf("\nstr1 is less than str2\n");
+	else
+		printf("\nstr1 is greater than str2\n");
+
+
+	strncpy(str3, str2, 3);
+
+	printf("\nString 3 value: %s\n", str3);
+
 
 	return 0;
 	
