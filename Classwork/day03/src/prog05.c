ead19810320004fb0886eb7fca197e33c40cf8f8
bhimatak <bhima.t@gmail.com>
2022-11-03 09:56:36 +0530

    Updated

--- Classwork/day03/src/prog05.c
+++ Classwork/day03/src/prog05.c
@@ -26,5 +26,23 @@ int main()
 int main()
 {
 	int resMulTab[10];
-	
+
+	int Val = 2, it;
+	int *arrPtr;
+
+	arrPtr = resMulTab;
+
+	for(it=0;it<10;it++)
+	{
+		resMulTab[it] = Val*(it+1);
+	}
+
+
+	for(it=0;it<10;it++,arrPtr++)
+	{
+		printf("\n%d\n",*arrPtr);
+	}
+
+	return 0;
+
 }
