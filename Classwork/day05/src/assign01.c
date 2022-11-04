17f8f2d788bccbb21065331f3fef700cfb018843
bhimatak <bhima.t@gmail.com>
2022-11-04 14:49:49 +0530

    Updated

--- Classwork/day05/src/assign01.c
+++ Classwork/day05/src/assign01.c
@@ -4,51 +4,82 @@
 
 #define MAX_BUFF 512
 
-int StrSortAsc(char **, int);
+int StrSortAsc(char *[], int);
+char ** copyStr(char **, char**, int);
 void displayStr(char **, int);
 
 
 int main(int argc, char *argv[])
 {
+	char **Names = NULL;
+	char **srcStr = NULL;
+	int i,j;
 
-	printf("\nBefore Sorting\n");
-	displayStr(argv, argc);
-	printf("\n\n");
-	StrSortAsc(argv, argc);
-	printf("\nAfter Sorting\n");
-	displayStr(argv, argc);
+	Names = (char **)malloc(50); // we are allocating rows first
+	
+	for(i=0;i<50;i++)
+		Names[i] = (char *)malloc(100);
+	if(copyStr(argv, Names, argc)!=NULL){
+	
+		printf("\nBefore Sorting\n");
+		
+		displayStr(Names, argc);
+		printf("\n\n");
+		StrSortAsc(Names, argc);
+		printf("\nAfter Sorting\n");
+		displayStr(Names, argc);
+	}
+	else
+		printf("\nSource String is Empty\n");
 
 	printf("\n\n");
 	return 0;
 }
 
 
-int StrSortAsc(char *Names[], int count)
+int StrSortAsc(char *name[], int n)
 {
 	int i,j;
 	char strTemp[MAX_BUFF] = {'\0', };
 
-	for(i=1;i<count;i++)
-	{
-		for(j=i+1;j<count;j++)
-		{
-			bzero(strTemp, MAX_BUFF);
-			if(strcmp(Names[i], Names[j])>0)
-			{
-				strcpy(strTemp, Names[i]);
-				strcpy(Names[i], Names[j]);
-				strcpy(Names[j], strTemp);
+	for (i = 0; i < n - 1 ; i++)
+        {
+        	bzero(strTemp, MAX_BUFF);
+            for (j = i + 1; j < n; j++)
+            {
+                if (strcmp(name[i], name[j]) > 0) 
+                {
+                    strcpy(strTemp, name[i]);
+                    strcpy(name[i], name[j]);
+                    strcpy(name[j], strTemp);
+                }
+            }
+        }
+}
 
-			}
-		}
+
+char ** copyStr(char **srcStr, char**descStr, int Cap)
+{
+	int i;
+	if(srcStr == NULL){
+		
+		return NULL;
 	}
+	for(i=1;i<Cap;i++)
+		bzero(descStr[i],100);
+	for(i=1;i<Cap;i++)
+		strcpy(descStr[i-1],srcStr[i]);
+		
+	
+
+	return descStr;
 }
 
 void displayStr(char *Names[], int count)
 {
 	int iv;
 	printf("\nNames are, \n");
-	for(iv = 1; iv<count;iv++)
+	for(iv = 0; iv<count;iv++)
 		printf("\n%s",Names[iv]);
 
 }
