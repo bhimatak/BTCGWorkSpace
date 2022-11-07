4a2404be34590b813d02b16ea6be65a6a51caae6
bhimatak <bhima.t@gmail.com>
2022-11-07 17:42:14 +0530

    Updated

--- Classwork/day06/inc/EmpStruct.h
+++ Classwork/day06/inc/EmpStruct.h
@@ -16,6 +16,6 @@ void dispEmp(struct EmpStruct [], int);
 
 void getEmpDetails(struct EmpStruct[], int);
 
-int findEmpDetails(struct EmpStruct [], char *);
+int findEmpDetails(struct EmpStruct [], char *, int);
 
 #endif
