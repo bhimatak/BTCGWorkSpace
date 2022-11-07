c9ce48f3711c647aee4ee56931ae6425694ddfbf
bhimatak <bhima.t@gmail.com>
2022-11-07 11:00:12 +0530

    Updated

--- Classwork/day06/src/main.c
+++ Classwork/day06/src/main.c
@@ -9,8 +9,9 @@ int main()
 	scanf("%d", &E1.eID);
 	printf("\n\tName: ");
 	scanf("%s", E1.eName);
+	getc(stdin);
 	printf("\n\tGender (M/F/O): ");
-	scanf("%c", &E1.eGender)
+	scanf("%c", &E1.eGender);
 	printf("\n\tAddress: ");
 	scanf("%s", E1.eAddr);
 	printf("\n\tPhone Number: ");
@@ -23,7 +24,7 @@ int main()
 	printf("\n\tName: ");
 	printf("%s", E1.eName);
 	printf("\n\tGender (M/F/O): ");
-	printf("%c", E1.eGender)
+	printf("%c", E1.eGender);
 	printf("\n\tAddress: ");
 	printf("%s", E1.eAddr);
 	printf("\n\tPhone Number: ");
