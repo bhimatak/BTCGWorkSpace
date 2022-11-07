24d2c9476c7f68e44fc8a1b38843592273c009c8
bhimatak <bhima.t@gmail.com>
2022-11-07 12:41:38 +0530

    Updated

---
+++ Classwork/day06/src/main3.c
@@ -0,0 +1,53 @@
+/* Array of Structures */
+
+
+#include <common.h>
+#include <EmpStruct.h>
+
+int main()
+{
+	struct EmpStruct E[SIZE];
+	struct EmpStruct *ePtr;
+	int it;
+	int iv;
+	int NoOfEmps = 3;
+
+	//EPTR should point to the BA of emp
+	printf("\nSize of the EmpStruct=%ld\n", sizeof(struct EmpStruct));
+	ePtr = E;
+
+	for(it =0,iv=0 ;it<NoOfEmps;it++, ePtr++){
+
+		printf("\n\tEnter %d Employee Details, \n",(iv+1));
+		printf("\n\tID: ");
+		scanf("%d", &E[iv].eID);
+		printf("\n\tName: ");
+		scanf("%s", E[iv].eName);
+		getc(stdin);
+		printf("\n\tGender (M/F/O): ");
+		scanf("%c", &E[iv].eGender);
+		printf("\n\tAddress: ");
+		scanf("%s", E[iv].eAddr);
+		printf("\n\tPhone Number: ");
+		scanf("%d",&E[iv].ePhone);
+	}
+
+	for(it=0;it<NoOfEmps;it++, ePtr++){
+		printf("\n\t%d Employee Details, \n",(it+1));
+		printf("\n\tID: ");
+		printf("%d", ePtr->eID);
+		printf("\n\tName: ");
+		printf("%s", ePtr->eName);
+		printf("\n\tGender (M/F/O): ");
+		printf("%c", ePtr->eGender);
+		printf("\n\tAddress: ");
+		printf("%s", ePtr->eAddr);
+		printf("\n\tPhone Number: ");
+		printf("%d",ePtr->ePhone);
+	}
+
+	printf("\n\n");
+	return 0;
+
+	
+}
