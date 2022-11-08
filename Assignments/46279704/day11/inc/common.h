c587cfc560544d1907d817733eb32f8068c0569b
charishma <nukala.charishma@capgemini.com>
2022-11-09 00:37:41 +0530

    updated assignments

---
+++ Assignments/46279704/day11/inc/common.h
@@ -0,0 +1,21 @@
+/*************************************************************************************************************
+ * 
+ * * FILE NAME : common.h
+ * *
+ * * DESCRIPTION : The file defines the function which send external message to the server
+ * *
+ * * REVISION HISTORY :
+ * *
+ * * DATE                    NAME                         REFERENCE                              REASON
+ * * --------------------------------------------------------------------------------------------------------
+ * *  06 OCT 2022         Charishma                        CAPG123                            new code
+ * *
+ * ************************************************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
