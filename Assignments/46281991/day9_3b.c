5cb890ca40b6f4afb1211b637b2470321d7f913e
Shivanisinhacg <shivanisinha790@gmail.com>
2022-11-08 17:05:08 +0530

    updated

---
+++ Assignments/46281991/day9_3b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 20
+
+char *swap_elements(char arrl[][MAX_LEN], int index);
+
+int isVowel(char x);
+
+char *remove_vowel(char arrl[][MAX_LEN], int index);
+
+int display(char arrl[][MAX_LEN]);
