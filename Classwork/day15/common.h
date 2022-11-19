ad6932d228b60d57916c9e1fab0a00d1c5091ded
bhimatak <bhima.t@gmail.com>
2022-11-19 23:58:14 +0530

    Updated

--- Classwork/day15/common.h
+++ Classwork/day15/common.h
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 
 typedef struct personalDetails
 {
 	int _id;
 	char _name[30];
 	char _gender;
+	struct personalDetails *next;
 
 }PD;
 
@@ -18,18 +21,25 @@ typedef struct loginDetails
 	int _id;
 	char _uName[30];
 	char _passwd[30];
+	struct loginDetails *next;
 }LD;
 
-
-PD * signUp(PD *);
-int singIn(LD *);
+PD *loadPD();
+LD *loadLD();
+PD* signUp(PD *, int*);
+LD* signInDetails(LD *, int);
+int signIn(LD *);
 int readPD(PD *);
 int writePD(PD *);
 int readLD(LD *);
 int writeLD(LD *);
 
 void dispPD(PD *);
+void dispLD(LD *);
 int tokenizePD(PD *, char *);
+int tokenizeLD(LD *, char *);
 
+void removeLeading(char *, char*);
+void removeTrailing(char *);
 
 #endif
