89e1434ad34aa5ddff74413d908e750511e4e0a1
bhimatak <bhima.t@gmail.com>
2022-11-19 23:58:14 +0530

    Updated

--- Classwork/day15/configMenu.h
+++ Classwork/day15/configMenu.h
@@ -1,2 +1,2 @@
 char WelCome[] = "\n\t=========================Welcome=========================\n";
-char MainMenu[] = "\n\tPress,\n\t1. SignUp\n\t2. SingIn\n\t3. Exit\n\tChoice: ";
+char MainMenu[] = "\n\tPress,\n\t1. SignUp\n\t2. SingIn\n\t3. Display All PD\n\t4. Display All LD\n\t5. Exit\n\tChoice: ";
