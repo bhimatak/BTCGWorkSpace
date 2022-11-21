91e42fdcbf2d660c697df54405e0c34e41f3a6d8
root <root@DESKTOP-K2TO9R0.localdomain>
2022-11-21 10:57:55 +0530

    updatec Assignments

---
+++ Assignments/46281996/day14-15/inc/common.h
@@ -0,0 +1,14 @@
+/*HEADER INFORMATION*************
+ * *
+ * *
+ * ******************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 80
+#endif/*End of _COMMON_H macro*/
