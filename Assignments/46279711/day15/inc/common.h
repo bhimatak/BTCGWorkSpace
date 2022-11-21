f33cbfaf1d67519b726d9f0f38aa9da58c26ec62
Polasutejcg <117061118+Polasutejcg@users.noreply.github.com>
2022-11-21 09:46:09 +0530

    Add files via upload

---
+++ Assignments/46279711/day15/inc/common.h
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ ** NAME  : common.h
+ **
+ ** DESCRIPTION : This file defines the function which send external message for Refer.
+ **
+ ** Revision History :
+ ** DATE            NAME              REFERENCE            REASON
+ ** ----------------------------------------------------------------------
+ **  13 NOV 2022    TEJA SRI           STRUCT                 NEW CODE
+ **
+ **
+ ** Copyright @ 2022 Capgemini All Rights Reserved
+ **
+ *******************************************************************************************/
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 100
+
+#endif
