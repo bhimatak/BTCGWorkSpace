b0c820a1aac565dbb958b72d41b7730c0d5402e7
gowrilatha579 <gowrilatha579@gmail.com>
2022-11-22 14:47:01 +0530

    update

---
+++ Assignments/46279726/Assignment10b/inc/common.h
@@ -0,0 +1,16 @@
+/************************************************************************************************************
+ *
+ * FILE NAME   :  common.h
+ *
+ * DESCRIPTION :  This file defines the function which sends external message for server
+ *
+ * HISTORY   :
+ *
+ * DATE                  NAME                       REFERENCE                             REASON
+ * 05 NOV 2022        GOWRILATHA CHOPPA             CG MATERIAL                            NEW
+ *
+ * Copyright 2010, Aricent Technologies (Holding) Ltd
+ * ******************************************************************************************************************/
+ #include <stdio.h>
+ #define MAX_LENGTH 5
+ #define MAX_COL 3
