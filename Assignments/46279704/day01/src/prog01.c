dc40306b70ff3fcac9a58b6ddba6f479cae55473
charishma <nukala.charishma@capgemini.com>
2022-11-22 12:27:42 +0530

    updated

---
+++ Assignments/46279704/day01/src/prog01.c
@@ -0,0 +1,27 @@
+/*
+This is my first Program
+DOC: 31-Oct-2022
+Author: BT
+Version: 0.1v
+*/
+
+#include <common.h>
+
+int main()
+{
+	float PI = 3.142;
+	int it;
+	char name[MAX] = "Bhima";
+
+	it = 101;
+
+	printf("\nPI = %f\tIT=%d",PI,it);
+	
+	printf("\nName: %s", name);
+	display();
+
+	return 0;
+}
+
+
+
