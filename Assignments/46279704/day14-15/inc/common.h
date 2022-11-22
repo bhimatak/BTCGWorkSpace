60698381b5056de3b32460934cb7749515756321
charishma <nukala.charishma@capgemini.com>
2022-11-22 12:57:18 +0530

    updated

---
+++ Assignments/46279704/day14-15/inc/common.h
@@ -0,0 +1,20 @@
+/* FILE  NAME : prog02.c
+ * *
+ * * DESCRIPTION : it sends the external message to he server
+ * *
+ * * REVISION HISTORY:
+ * *
+ * * DATE                 NAME                     REFERENCE               REASON
+ * * --------------------------------------------------------------------------------
+ * * 05 Nov  2022       Charishma                   SP101                New code
+ * *
+ * * **********************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 80
+#endif
