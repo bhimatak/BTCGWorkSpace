097b329eb7967b2567603a5462321daeaa9a6131
trivenimamidi <mamidi.triveni@capgemini.com>
2022-11-22 16:52:05 +0530

    updated

---
+++ Assignments/46279727/ass17/inc/common.h
@@ -0,0 +1,24 @@
+/*********************************
+ * *
+ * * FILE NAME   : common.h
+ * *
+ * * DESCRIPTION : program to demontrate the quick sort using strings
+ * *
+ * *Revision History:
+ * * DATE 		NAME   		REFERNCE 		REASON
+ * * 
+ * * 17-11-2022        trivenimamidi         new               Employee referal
+ * *
+ * * @Copyright 2022 All Rights Reserved
+ ****************************************************
+ #ifdef COMMON_H
+ #define COMMON_H
+ #define MAX 80
+ * *************************************************
+ * *				STANDARD HEADER FILES
+ * * *************************************************/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#endif/*end of common.h*/
