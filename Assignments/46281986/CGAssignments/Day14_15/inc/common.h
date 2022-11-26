c8cf922c2fa238f19f2b2f1db18c4700b7221768
ranjita1307 <sushree-ranjita.panda@capgemini.com>
2022-11-26 10:12:06 +0530

    updated

---
+++ Assignments/46281986/CGAssignments/Day14_15/inc/common.h
@@ -0,0 +1,16 @@
+/***********************************************************************************************************
+ * * FILENAME: common.h
+ * *
+ * * DESCRIPTION: This file contains required libraries and macros
+ * *
+ * * REVISION HISTORY:
+ * * DATE                  NAME                    REFERENCE                          REASON
+ * * ------------------------------------------------------------------------------------------------------------
+ * * 5 Nov 2022      Sushree Ranjita Panda             NEW                  New code to perform given operations  
+ * **********************************************************************************************************/
+#ifndef _COMMON_H
+#define _COMMON_H
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#endif    /*end of ifndef _COMMON_H*/
---
+++ Assignments/46281986/CGAssignments/Day16_17/inc/common.h
@@ -0,0 +1,18 @@
+/*******************************************************************************************************************
+* FILENAME        : common.h
+* DESCRIPTION     : This file contains required header files and macros
+* REVISION HISTORY:
+* DATE                NAME                       REFERENCE                       REASON
+* -----------------------------------------------------------------------------------------------------------------
+*  16 Nov 2022    Sushree Ranjita                 NEW                          NEW CODE TO PERFORM GIVEN OPERATION
+*
+*********************************************************************************************************/
+#ifndef COMMON_H
+#define COMMON_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 80
+
+#endif
